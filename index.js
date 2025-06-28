244d1741e867de3467fd2018b68bab8ebfe35106
m-falme <mburumark096@gmail.com>
2025-06-29 00:59:25 +0300

    removed  comments

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// let saveEl= document.getElementById("save-el")
+ let saveEl= document.getElementById("save-el")
+ let countEl = document.getElementById("count-el")
+ count = 0
 
-// let countEl = document.getElementById("count-el")
-// count = 0
-
-// function increment(){
-//      count += 1;
-//      countEl.innerText = count
-// }
-
+ function increment(){
+      count += 1;
+      countEl.innerText = count
+ }
 
 
-// function save(){
-//      let countStr = count + " - "
-//      saveEl.textContent += countStr
-//      countEl.textContent = 0
-//      count = 0
-// }
+
+ function save(){
+      let countStr = count + " - "
+      saveEl.textContent += countStr
+      countEl.textContent = 0
+      count = 0
+ }
 
 
 
